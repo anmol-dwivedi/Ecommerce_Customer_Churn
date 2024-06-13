093c97bae1504725246af3cc01f019561a6bd7e3
Anmol Dwivedi <dwivedi.anmol1996@gmail.com>
2024-06-13 16:03:53 -0500

    Add files via upload
    Updated files for the streamlit app deployment.

--- churn_app.py
+++ churn_app.py
@@ -3,17 +3,16 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
-import time
-import datetime
 import warnings
 warnings.filterwarnings('ignore')
 
 import pickle
 import streamlit as st
+import shap
 import xgboost as xgb
-from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
 
-#####################################################################################################################################################################################################
+from churn_app_functions import model_performance, roc_score_auc_curve, k_fold_cross_valscore, plot_shap_summary, plot_partial_dependence, plot_calibration_curve, plot_learning_curve
+
 # Load the Models
 def load_model(path):
     try:
@@ -48,133 +47,182 @@ def preprocess_input(data):
     # Concatenate Features
     data_final = np.hstack((data_processed, kmeans_labels))
     
-    
-    # recreate the dataframe
+    # Recreate the dataframe
     all_columns = [
-    'Tenure', 'CityTier', 'WarehouseToHome', 'HourSpendOnApp', 'NumberOfDeviceRegistered', 
-    'SatisfactionScore', 'NumberOfAddress', 'Complain', 'OrderAmountHikeFromlastYear', 
-    'CouponUsed', 'OrderCount', 'DaySinceLastOrder', 'CashbackAmount', 'PreferredLoginDevice_Phone', 
-    'PreferredPaymentMode_COD', 'PreferredPaymentMode_DC', 'PreferredPaymentMode_UPI', 
-    'Gender_Male', 'PreferedOrderCat_Grocery', 'PreferedOrderCat_Laptop_adons', 'PreferedOrderCat_Mobile',
-    'PreferedOrderCat_Others','MaritalStatus_Married', 'MaritalStatus_Single', 'KMeansCluster']
+        'Tenure', 'CityTier', 'WarehouseToHome', 'HourSpendOnApp', 'NumberOfDeviceRegistered', 
+        'SatisfactionScore', 'NumberOfAddress', 'Complain', 'OrderAmountHikeFromlastYear', 
+        'CouponUsed', 'OrderCount', 'DaySinceLastOrder', 'CashbackAmount', 'PreferredLoginDevice_Phone', 
+        'PreferredPaymentMode_COD', 'PreferredPaymentMode_DC', 'PreferredPaymentMode_UPI', 
+        'Gender_Male', 'PreferedOrderCat_Grocery', 'PreferedOrderCat_Laptop_adons', 'PreferedOrderCat_Mobile',
+        'PreferedOrderCat_Others','MaritalStatus_Married', 'MaritalStatus_Single', 'KMeansCluster'
+    ]
     
     modelling_data = pd.DataFrame(data_final, columns=all_columns)
     
     return modelling_data
 
-#######################################################################################################################################################################################################
-st.title(""" # E-commerce Churn Prediction """)
-st.write('---')
+# Load the data
+data_dir = '/home/flame/Desktop/Projects/Churn Prediction/data/'
+x_train = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
+x_test = pd.read_csv(os.path.join(data_dir, 'x_test.csv'))
+y_train = pd.read_csv(os.path.join(data_dir, 'y_train.csv'))
+y_test = pd.read_csv(os.path.join(data_dir, 'y_test.csv'))
 
-# Sidebar (User Input Pane)
-st.sidebar.title('Input Features')
-input_data = {
-'Tenure' : st.sidebar.number_input('Tenure of Customer', min_value=0, max_value=70, value=3),
-'CityTier': st.sidebar.slider('Select the City Tier', min_value=1, max_value=3, value=2),
-'WarehouseToHome' : st.sidebar.number_input('Warehouse to Home Distance', min_value=1, max_value=200, value=30),
-'HourSpendOnApp' : st.sidebar.slider('Hours Spent on the App', min_value=0, max_value=10, value=2),
-'NumberOfDeviceRegistered' : st.sidebar.slider('Number of Registered Devices', min_value=1, max_value=10),
-'SatisfactionScore' : st.sidebar.select_slider('Satisfaction Score', [1,2,3,4,5], value=4),
-'NumberOfAddress' : st.sidebar.number_input('Number of Address', min_value=1, max_value=30, value=2),
-'Complain' : st.sidebar.radio('Any Complaints', [1,0]),
-'OrderAmountHikeFromlastYear' : st.sidebar.number_input('Order Amount Hike from Last Year', min_value=1, max_value=50, value=3),
-'CouponUsed' : st.sidebar.number_input('Number of Coupons Used', min_value=0, max_value=50, value=2),
-'OrderCount' : st.sidebar.number_input('Number of Orders', min_value=1, max_value=30, value=2),
-'DaySinceLastOrder' : st.sidebar.number_input('Number of Days since last order', min_value=1, max_value=100, value=10),
-'CashbackAmount' : st.sidebar.slider('Cashback Amount', min_value=0, max_value=500, value=100),
-'PreferredLoginDevice' : st.sidebar.radio('Preferred Login Device', ['Phone', 'Computer']),   
-'PreferredPaymentMode' : st.sidebar.radio('Preferred Payment Model', ['DC', 'CC','UPI', 'CC']),  
-'Gender' : st.sidebar.radio('Gender', ['Female',  'Male']), 
-'PreferedOrderCat' : st.sidebar.radio('Preferred Order Category', ['Mobile','Grocery','Laptop_adons', 'Others',  'Fashion']), 
-'MaritalStatus' : st.sidebar.radio('Marital Status', ['Single', 'Married','Divorced']) 
-}
+# Function to show the home page
+def show_home_page():
+    st.title("Churn Prediction Home") 
+    st.write("")
+    st.write("")
+    st.header('Input Features')
 
-# Convert input data to DataFrame
-input_df = pd.DataFrame([input_data])
 
-# Display the input DataFrame
-st.subheader('User Inputs')
-st.write(input_df)
-# st.write(input_df.columns)
-# st.write(input_df.dtypes)
+    col1, col2, col3 = st.columns(3)
+    input_data = {}
 
-#######################################################################################################################################################################################################
+    with col1:
+        input_data['Tenure'] = st.number_input('Tenure of Customer', min_value=0, max_value=70, value=5)
+        input_data['CityTier'] = st.radio('Select the City Tier', [1, 2, 3])
+        input_data['WarehouseToHome'] = st.number_input('Warehouse to Home Distance', min_value=1, max_value=200, value=30)
+        input_data['HourSpendOnApp'] = st.slider('Hours Spent on the App', min_value=0, max_value=10, value=2)
+        input_data['NumberOfDeviceRegistered'] = st.slider('Number of Registered Devices', min_value=1, max_value=10)
+        input_data['SatisfactionScore'] = st.select_slider('Satisfaction Score', [1, 2, 3, 4, 5], value=4)
 
-# Define categorical columns and their mappings
-categorical_cols = ['PreferredLoginDevice', 'PreferredPaymentMode', 'Gender', 'PreferedOrderCat', 'MaritalStatus']
-one_hot_mappings = {
-    'PreferredLoginDevice': ['PreferredLoginDevice_Phone'],
-    'PreferredPaymentMode': ['PreferredPaymentMode_COD', 
-                             'PreferredPaymentMode_DC',
-                             'PreferredPaymentMode_UPI', 
-                             'PreferredPaymentMode_CC'],
-    'Gender': ['Gender_Male'],
-    'PreferedOrderCat': ['PreferedOrderCat_Grocery', 
-                         'PreferedOrderCat_Laptop_adons',
-                         'PreferedOrderCat_Mobile',
-                         'PreferedOrderCat_Others',
-                         'PreferedOrderCat_Fashion'],
-    'MaritalStatus': ['MaritalStatus_Married', 
-                      'MaritalStatus_Single', 
-                      'MaritalStatus_Divorced']
-}
+    with col2:
+        input_data['NumberOfAddress'] = st.number_input('Number of Address', min_value=1, max_value=30, value=2)
+        input_data['Complain'] = st.radio('Any Complaints', [0, 1])
+        input_data['OrderAmountHikeFromlastYear'] = st.number_input('Order Amount Hike from Last Year', min_value=1, max_value=50, value=15)
+        input_data['CouponUsed'] = st.number_input('Number of Coupons Used', min_value=1, max_value=50, value=2)
+        input_data['OrderCount'] = st.number_input('Number of Orders', min_value=1, max_value=30, value=2)
+        input_data['DaySinceLastOrder'] = st.number_input('Number of Days since last order', min_value=1, max_value=100, value=10)
+        input_data['MaritalStatus'] = st.radio('Marital Status', ['Married', 'Single', 'Divorced'])
+        
 
-# Apply one-hot encoding and ensure all required columns are present
-for col in categorical_cols:
-    for category in one_hot_mappings[col]:
-        input_df[category] = (input_df[col] == category.split('_')[1]).astype(int)
-    input_df.drop(columns=[col], inplace=True)
+    with col3:
+        input_data['CashbackAmount'] = st.slider('Cashback Amount', min_value=0, max_value=500, value=150)
+        input_data['PreferredLoginDevice'] = st.radio('Preferred Login Device', ['Phone', 'Computer'])
+        input_data['PreferredPaymentMode'] = st.radio('Preferred Payment Mode', ['COD', 'DC', 'UPI', 'CC'])
+        input_data['Gender'] = st.radio('Gender', ['Male', 'Female'])
+        input_data['PreferedOrderCat'] = st.radio('Preferred Order Category', ['Grocery', 'Laptop_adons', 'Mobile', 'Others', 'Fashion'])
+
+
+    if st.button('Predict'):
+        input_df = pd.DataFrame([input_data])
+
+        # Define categorical columns and their mappings
+        categorical_cols = ['PreferredLoginDevice', 'PreferredPaymentMode', 'Gender', 'PreferedOrderCat', 'MaritalStatus']
+        one_hot_mappings = {
+            'PreferredLoginDevice': ['PreferredLoginDevice_Phone'],
+            'PreferredPaymentMode': ['PreferredPaymentMode_COD', 'PreferredPaymentMode_DC', 'PreferredPaymentMode_UPI', 'PreferredPaymentMode_CC'],
+            'Gender': ['Gender_Male'],
+            'PreferedOrderCat': ['PreferedOrderCat_Grocery', 'PreferedOrderCat_Laptop_adons', 'PreferedOrderCat_Mobile', 'PreferedOrderCat_Others', 'PreferedOrderCat_Fashion'],
+            'MaritalStatus': ['MaritalStatus_Married', 'MaritalStatus_Single', 'MaritalStatus_Divorced']
+        }
+
+        # Apply one-hot encoding and ensure all required columns are present
+        for col in categorical_cols:
+            for category in one_hot_mappings[col]:
+                input_df[category] = (input_df[col] == category.split('_')[1]).astype(int)
+            input_df.drop(columns=[col], inplace=True)
+        
+        # Ensure all columns are present
+        required_columns = [
+            'Tenure', 'CityTier', 'WarehouseToHome', 'HourSpendOnApp', 'NumberOfDeviceRegistered',
+            'SatisfactionScore', 'NumberOfAddress', 'Complain', 'OrderAmountHikeFromlastYear',
+            'CouponUsed', 'OrderCount', 'DaySinceLastOrder', 'CashbackAmount', 'PreferredLoginDevice_Phone',
+            'PreferredPaymentMode_COD', 'PreferredPaymentMode_DC', 'PreferredPaymentMode_UPI',
+            'Gender_Male', 'PreferedOrderCat_Grocery', 'PreferedOrderCat_Laptop_adons', 'PreferedOrderCat_Mobile',
+            'PreferedOrderCat_Others', 'MaritalStatus_Married', 'MaritalStatus_Single'
+        ]
+        
+        for col in required_columns:
+            if col not in input_df.columns:
+                input_df[col] = 0
+
+        st.write("")  # Add a line space
+        st.write("")
+        # st.write("")
+        # st.subheader('User Input Values')
+        # col1, col2 = st.columns(2)
+        # with col1:
+        #     st.write(f"**Tenure**: {input_data['Tenure']}")
+        #     st.write(f"**CityTier**: {input_data['CityTier']}")
+        #     st.write(f"**WarehouseToHome**: {input_data['WarehouseToHome']}")
+        #     st.write(f"**HourSpendOnApp**: {input_data['HourSpendOnApp']}")
+        #     st.write(f"**NumberOfDeviceRegistered**: {input_data['NumberOfDeviceRegistered']}")
+        #     st.write(f"**SatisfactionScore**: {input_data['SatisfactionScore']}")
+        #     st.write(f"**NumberOfAddress**: {input_data['NumberOfAddress']}")
+        #     st.write(f"**Complain**: {input_data['Complain']}")
+        #     st.write(f"**OrderAmountHikeFromlastYear**: {input_data['OrderAmountHikeFromlastYear']}")
+        #     st.write(f"**CouponUsed**: {input_data['CouponUsed']}")
+
+        # with col2:
+        #     st.write(f"**OrderCount**: {input_data['OrderCount']}")
+        #     st.write(f"**DaySinceLastOrder**: {input_data['DaySinceLastOrder']}")
+        #     st.write(f"**CashbackAmount**: {input_data['CashbackAmount']}")
+        #     st.write(f"**PreferredLoginDevice**: {input_data['PreferredLoginDevice']}")
+        #     st.write(f"**PreferredPaymentMode**: {input_data['PreferredPaymentMode']}")
+        #     st.write(f"**Gender**: {input_data['Gender']}")
+        #     st.write(f"**PreferedOrderCat**: {input_data['PreferedOrderCat']}")
+        #     st.write(f"**MaritalStatus**: {input_data['MaritalStatus']}")
+
+        # Preprocess input data
+        input_preprocessed = preprocess_input(input_df)
+
+        st.write("")
+        st.write("")  
+        st.subheader('Prediction')
+        # Make prediction
+        prediction = final_model.predict(input_preprocessed)
+        prediction_proba = final_model.predict_proba(input_preprocessed)
+
+        # Display prediction
+        if prediction[0] == 1:
+            st.markdown('<p style="color:red;font-weight:bold; font-size:20px;">Churn</p>', unsafe_allow_html=True)
+        else:
+            st.markdown('<p style="color:green;font-weight:bold; font-size:20px;">No Churn</p>', unsafe_allow_html=True)
+
+        st.write("")  # Add a line space
+        st.write("")
+        st.subheader('Prediction Probability')
+        st.write(f'Probability of Churn: {prediction_proba[0][1]:.4f}')
+        st.write(f'Probability of No Churn: {prediction_proba[0][0]:.4f}')
+
+# Function to show the model information page
+def show_model_info():
+    st.title("Model Information")
     
-# st.write(input_df)
-# st.write(input_df.columns)
-# st.write(input_df.dtypes)
-    
-######################################################################################################################################################################################################
+    st.write("### Model Performance")
+    model_performance(final_model, x_train, y_train, x_test, y_test)
 
-# Ensure all columns are present
-required_columns = [
-    'Tenure', 'CityTier', 'WarehouseToHome', 'HourSpendOnApp', 'NumberOfDeviceRegistered', 
-    'SatisfactionScore', 'NumberOfAddress', 'Complain', 'OrderAmountHikeFromlastYear', 
-    'CouponUsed', 'OrderCount', 'DaySinceLastOrder', 'CashbackAmount', 'PreferredLoginDevice_Phone', 
-    'PreferredPaymentMode_COD', 'PreferredPaymentMode_DC', 'PreferredPaymentMode_UPI', 
-    'Gender_Male', 'PreferedOrderCat_Grocery', 'PreferedOrderCat_Laptop_adons', 'PreferedOrderCat_Mobile',
-    'PreferedOrderCat_Others','MaritalStatus_Married', 'MaritalStatus_Single']
+    st.write("")  # Add a line space
+    st.write("### ROC AUC Curve")
+    roc_score_auc_curve(final_model, x_train, y_train, x_test, y_test)
 
-# for col in required_columns:
-#     if col not in input_df.columns:
-#         input_df[col] = 0
+    st.write("")  # Add a line space
+    st.write("### K-Fold Cross Validation Scores")
+    k_fold_cross_valscore(final_model, x_train, y_train, folds=10)
 
-# Keep only the required columns
-input_df = input_df[required_columns]
+    st.write("")  # Add a line space
+    st.write("### SHAP Summary Plots")
+    plot_shap_summary(final_model, x_test)
 
+    st.write("")  # Add a line space
+    st.write("### Partial Dependence Plots")
+    plot_partial_dependence(final_model, x_test)
 
-# st.write(input_df)
-# st.write(input_df.columns)
-# st.write(input_df.dtypes)
-#######################################################################################################################################################################################################
+    st.write("")  # Add a line space
+    st.write("### Calibration Curve")
+    plot_calibration_curve(final_model, x_test, y_test)
 
-# # Display the input DataFrame
-# st.subheader('Processed DataFrame')
-# processed_df = input_df
-# st.write(processed_df)
-# st.write(processed_df.columns)
+    st.write("")  # Add a line space
+    st.write("### Learning Curve")
+    plot_learning_curve(final_model, x_train, y_train)
 
-# # Preprocess input data
-input_preprocessed = preprocess_input(input_df)
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Model Information"])
 
-# # Make prediction
-prediction = final_model.predict(input_preprocessed)
-prediction_proba = final_model.predict_proba(input_preprocessed)
-
-# Display prediction
-st.subheader('Prediction')
-prediction_text = 'Customer at Risk of Churn' if prediction[0] == 1 else 'Happy Customer'
-prediction_color = 'red' if prediction[0] == 1 else 'green'
-st.markdown(f"<h2 style='color:{prediction_color};font-weight:bold;'>{prediction_text}</h2>", unsafe_allow_html=True)
-# st.write('Churn' if prediction[0] == 1 else 'No Churn')
-
-
-
-st.subheader('Prediction Probability')
-st.write(f'Probability of Churn: {prediction_proba[0][1]:.4f}')
-st.write(f'Probability of No Churn: {prediction_proba[0][0]:.4f}')
+if page == "Home":
+    show_home_page()
+else:
+    show_model_info()
---
+++ churn_app_functions.py
@@ -0,0 +1,165 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
+from sklearn.model_selection import cross_val_score
+from sklearn.calibration import calibration_curve
+from sklearn.inspection import PartialDependenceDisplay
+import shap
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+def model_performance(model_name, x_train_data, y_train_data, x_test_data, y_test_data):
+    train_preds = model_name.predict(x_train_data)
+    test_preds = model_name.predict(x_test_data)
+    
+    train_report = classification_report(y_train_data, train_preds)
+    test_report = classification_report(y_test_data, test_preds)
+    
+    train_score = round(model_name.score(x_train_data, y_train_data), 4)
+    test_score = round(model_name.score(x_test_data, y_test_data), 4)
+    
+    st.write('Classification report for training data')
+    st.text(train_report)
+    st.write('\n')
+    st.write('Classification report for testing data')
+    st.text(test_report)
+    st.write('\n')
+    st.write(f'The model score for training data is {train_score}')
+    st.write(f'The model score for testing data is {test_score}')
+    st.write('\n')
+
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Confusion Matrix for Testing Data", "Confusion Matrix for Training Data"))
+
+    test_cm = confusion_matrix(y_test_data, test_preds)
+    train_cm = confusion_matrix(y_train_data, train_preds)
+
+    fig.add_trace(
+        go.Heatmap(
+            z=test_cm,
+            x=["Predicted 0", "Predicted 1"],
+            y=["Actual 0", "Actual 1"],
+            colorscale="Viridis",
+            showscale=False,
+            text=test_cm,
+            texttemplate="%{text}"
+        ),
+        row=1, col=1
+    )
+
+    fig.add_trace(
+        go.Heatmap(
+            z=train_cm,
+            x=["Predicted 0", "Predicted 1"],
+            y=["Actual 0", "Actual 1"],
+            colorscale="Viridis",
+            showscale=False,
+            text=train_cm,
+            texttemplate="%{text}"
+        ),
+        row=1, col=2
+    )
+
+    fig.update_layout(title_text="Confusion Matrices")
+    st.plotly_chart(fig)
+
+def roc_score_auc_curve(model_name, x_train_data, y_train_data, x_test_data, y_test_data):
+    train_auc = round(roc_auc_score(y_train_data, model_name.predict_proba(x_train_data)[:,1]), 4)
+    test_auc = round(roc_auc_score(y_test_data, model_name.predict_proba(x_test_data)[:,1]), 4)
+    st.write(f'AUC Score for Model on Training Data is {train_auc}')
+    st.write(f'AUC Score for Model on Testing Data is {test_auc}')
+    
+    train_fpr, train_tpr, _ = roc_curve(y_train_data, model_name.predict_proba(x_train_data)[:,1])
+    test_fpr, test_tpr, _ = roc_curve(y_test_data, model_name.predict_proba(x_test_data)[:,1])
+    
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=train_fpr, y=train_tpr, mode='lines', name='Training', line=dict(color='blue', width=2)))
+    fig.add_trace(go.Scatter(x=test_fpr, y=test_tpr, mode='lines', name='Testing', line=dict(color='red', width=2)))
+    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', line=dict(dash='dash', color='gray'), name='No Skill'))
+
+    fig.update_layout(title='ROC Curve', xaxis_title='False Positive Rate', yaxis_title='True Positive Rate')
+    
+    st.plotly_chart(fig)
+
+
+def k_fold_cross_valscore(model_name, x_train_data, y_train_data, folds):
+    metrics = ['recall', 'accuracy', 'precision', 'f1']
+    scores = {metric: cross_val_score(model_name, x_train_data, y_train_data, cv=folds, scoring=metric, verbose=0)
+              for metric in metrics}
+    
+    cross_val_data = pd.DataFrame(scores)
+    
+    for metric in metrics:
+        st.write(f"The mean {metric} for the model after {folds} folds is {np.mean(scores[metric]):.4f}")
+    
+    st.write(cross_val_data)
+
+def plot_shap_summary(model, X_test):
+    explainer = shap.Explainer(model)
+    shap_values = explainer(X_test)
+    
+    fig, ax = plt.subplots()
+    shap.summary_plot(shap_values, X_test, plot_type='bar', show=False)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def plot_partial_dependence(model, X_test):
+    features = ['CashbackAmount', 'WarehouseToHome', 'OrderAmountHikeFromlastYear',
+                'Tenure', 'DaySinceLastOrder', 'SatisfactionScore',
+                'NumberOfDeviceRegistered', 'CouponUsed', 'OrderCount',
+                'Gender_Male', 'CityTier','PreferredPaymentMode_DC']
+
+    fig, ax = plt.subplots(nrows=len(features)//3 + 1, ncols=3, figsize=(18, len(features) * 2))
+    
+    display = PartialDependenceDisplay.from_estimator(model, X_test, features, grid_resolution=50, ax=ax.flatten()[:len(features)])
+    
+    for i, axi in enumerate(ax.flatten()[:len(features)]):
+        axi.set_xlabel('Feature value')
+        axi.set_ylabel('Partial dependence')
+        axi.set_title(f'Partial Dependence of {features[i]}')
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+def plot_calibration_curve(model, X_test, y_test):
+    y_prob = model.predict_proba(X_test)[:, 1]
+    prob_true, prob_pred = calibration_curve(y_test, y_prob, n_bins=10)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=prob_pred, y=prob_true, mode='markers+lines', name='Model'))
+    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', line=dict(dash='dash'), name='Perfectly calibrated'))
+
+    fig.update_layout(title='Calibration Curve', xaxis_title='Predicted probability', yaxis_title='True probability')
+
+    st.plotly_chart(fig)
+
+def plot_learning_curve(model, X_train, y_train):
+    from sklearn.model_selection import learning_curve
+
+    train_sizes, train_scores, val_scores = learning_curve(
+        model, X_train, y_train, cv=5, scoring='recall',
+        train_sizes=np.linspace(0.1, 1.0, 10), n_jobs=-1
+    )
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    val_scores_mean = np.mean(val_scores, axis=1)
+    val_scores_std = np.std(val_scores, axis=1)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=train_sizes, y=train_scores_mean, mode='lines+markers', name='Training Score', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=train_sizes, y=val_scores_mean, mode='lines+markers', name='Cross-Validation Score', line=dict(color='green')))
+    fig.add_trace(go.Scatter(x=train_sizes, y=train_scores_mean + train_scores_std, mode='lines', name='Train Score + STD', line=dict(dash='dash', color='red')))
+    fig.add_trace(go.Scatter(x=train_sizes, y=train_scores_mean - train_scores_std, mode='lines', name='Train Score - STD', line=dict(dash='dash', color='red')))
+    fig.add_trace(go.Scatter(x=train_sizes, y=val_scores_mean + val_scores_std, mode='lines', name='Val Score + STD', line=dict(dash='dash', color='green')))
+    fig.add_trace(go.Scatter(x=train_sizes, y=val_scores_mean - val_scores_std, mode='lines', name='Val Score - STD', line=dict(dash='dash', color='green')))
+
+    fig.update_layout(title='Learning Curve', xaxis_title='Training Examples', yaxis_title='Score')
+
+    st.plotly_chart(fig)
